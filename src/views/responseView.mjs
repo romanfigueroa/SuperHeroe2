b2eaca9248bc4afc7cb610169c43bd848ef798b9
romanfigueroa <134389908+romanfigueroa@users.noreply.github.com>
2024-12-07 15:22:25 -0300

    Update responseView.mjs

--- src/views/responseView.mjs
+++ src/views/responseView.mjs
@@ -1,17 +1,17 @@
 export function renderizarSuperheroe(superheroe) {
     return {
-        Nombre: superheroe.nombreSuperHeroe,
-        "Nombre Real": superheroe.nombreReal,
-        Edad: superheroe.edad,
-        "Planeta de Origen": superheroe.planetaOrigen,
-        Debilidad: superheroe.debilidad,
-        Poderes: superheroe.poderes,
-        Aliados: superheroe.aliados,
-        Enemigos: superheroe.enemigos,
-        autor: 'Acosta Javier Grupo-01'
-    };
-}
-
-export function renderizarListaSuperheroes(superheroes) {
+      Nombre: superheroe.nombreSuperheroe,
+      "Nombre Real": superheroe.nombreReal,
+      Edad: superheroe.edad,
+      "Planeta de Origen": superheroe.planetaOrigen,
+      Debilidad: superheroe.debilidad,
+      Poderes: superheroe.poderes,
+      Aliados: superheroe.aliados,
+      Enemigos: superheroe.enemigos
+    }
+  }
+  
+  export function renderizarListaSuperheroes(superheroes) {
     return superheroes.map(superheroe => renderizarSuperheroe(superheroe));
-}
+  }
+  
